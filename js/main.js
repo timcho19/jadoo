6ff9a2c2c9ce6d610281e70f4c46c5ceab1a2fa8
timcho19 <timcho4589@gmail.com>
2025-05-01 13:19:56 +0900

    main js 스크롤이벤트 설정

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const book_a_trip = document.querySelector(".book_a_trip");
+const book_a_triP_OST = book_a_trip.offsetTop;
+const book_a_trip_height = book_a_trip.offsetHeight;
+
+const ongoing_percent = document.querySelector(".ongoing .percent");
+const ongoing_bar = document.querySelector(".ongoing .bar");
+
+
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY - 300 > book_a_triP_OST - book_a_trip_height) {
+    book_a_trip.classList.add("active");
+    startNumberAnimation();
+
+  } else {
+    book_a_trip.classList.remove("active");
+    stopNumberAnimation();
+  }
+});
+
+function startNumberAnimation(){
+  let start = 0;
+  let end = Number(ongoing_bar.getAttribute('data-rate'));
+
+  setInterval(() => {
+    if (start < end) {
+      start++;
+      ongoing_percent.innerText = start + "%";
+      ongoing_bar.style.width = start + "%";
+    } else {
+      clearInterval(this);
+    }
+  }, 100);
+}
+
+
+
+
+
